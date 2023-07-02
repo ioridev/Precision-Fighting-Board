3a8b769b1a502b0793158acf1b34b88cb04f5139
Iori Kojima <iori@iori.dev>
2023-07-02 11:44:09 +0900

    rm: d-input

--- PrecisionFightingBoard.ino
+++ PrecisionFightingBoard.ino
@@ -1,55 +1,38 @@
-#define NUM_BUTTONS 15
-const uint8_t BUTTON_PINS[NUM_BUTTONS] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+#include <XInput.h>
 
-#define NUM_DPAD 4
-const uint8_t DPAD_PINS[NUM_DPAD] = {23, 22, 21, 20};  // Up, Right, Down, Left
-
-const int16_t HAT_MAP[16] = {
-  -1, // 0000 All dpad buttons up (CENTERED)
-  0, // 0001 direction UP
-  90, // 0010 direction RIGHT
-  45, // 0011 direction UP RIGHT
-  180, // 0100 direction DOWN
-  -1, // 0101 invalid
-  135, // 0110 direction DOWN RIGHT
-  -1, // 0111 invalid
-  270, // 1000 direction LEFT
-  315, // 1001 direction UP LEFT
-  -1, // 1010 invalid
-  -1, // 1011 invalid
-  225, // 1100 direction DOWN LEFT
-  -1, // 1101 invalid
-  -1, // 1110 invalid
-  -1 // 1111 invalid
-};
+#define NUM_BUTTONS 16
+const uint8_t BUTTON_PINS[NUM_BUTTONS] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
 
 void setup() {
-  // set up buttons
   for (int i = 0; i < NUM_BUTTONS; i++) {
     pinMode(BUTTON_PINS[i], INPUT_PULLUP);
   }
-  
-  // set up dpad
-  for (int i = 0; i < NUM_DPAD; i++) {
-    pinMode(DPAD_PINS[i], INPUT_PULLUP);
-  }
+  XInput.setAutoSend(false);
 }
 
 void loop() {
-  // read the digital inputs and set the buttons
-  for (int i = 0; i < NUM_BUTTONS; i++) {
-    Joystick.button(i + 1, !digitalRead(BUTTON_PINS[i]));  // active low buttons
-  }
+  bool up = !digitalRead(BUTTON_PINS[10]);
+  bool down = !digitalRead(BUTTON_PINS[11]);
+  bool left = !digitalRead(BUTTON_PINS[12]);
+  bool right = !digitalRead(BUTTON_PINS[13]);
 
-  // read the dpad states
-  uint8_t dpad_bits = 0;
-  for (unsigned i = 0; i < NUM_DPAD; i++) {
-    if (!digitalRead(DPAD_PINS[i])) {  // active low
-      dpad_bits |= (1 << i);
-    }
-  }
-  int16_t hatValue = HAT_MAP[dpad_bits];
-  Joystick.hat(hatValue);  // Set the hat switch to the calculated angle
+  XInput.setDpad(up, down, left, right);
 
- delayMicroseconds(125);
+  XInput.setButton(BUTTON_A, !digitalRead(BUTTON_PINS[0]));
+  XInput.setButton(BUTTON_B, !digitalRead(BUTTON_PINS[1]));
+  XInput.setButton(BUTTON_X, !digitalRead(BUTTON_PINS[2]));
+  XInput.setButton(BUTTON_Y, !digitalRead(BUTTON_PINS[3]));
+
+  XInput.setButton(BUTTON_LB, !digitalRead(BUTTON_PINS[4]));
+  XInput.setButton(BUTTON_RB, !digitalRead(BUTTON_PINS[5]));
+  XInput.setTrigger(TRIGGER_LEFT, !digitalRead(BUTTON_PINS[6])); // L2
+  XInput.setTrigger(TRIGGER_RIGHT, !digitalRead(BUTTON_PINS[7])); // R2
+  XInput.setButton(BUTTON_BACK, !digitalRead(BUTTON_PINS[8])); // SELECT
+  XInput.setButton(BUTTON_START, !digitalRead(BUTTON_PINS[9]));
+  XInput.setButton(BUTTON_L3, !digitalRead(BUTTON_PINS[14]));
+  XInput.setButton(BUTTON_R3, !digitalRead(BUTTON_PINS[15]));
+
+  XInput.send();
+
+  delayMicroseconds(125); // 800
 }
