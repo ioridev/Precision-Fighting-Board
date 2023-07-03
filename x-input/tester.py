54e1b2c6a36993159b87f3c438691f63e31fc755
Iori Kojima <iori@iori.dev>
2023-07-03 21:48:05 +0900

    fix tester

--- x-input/tester.py
+++ x-input/tester.py
@@ -2,11 +2,20 @@ from inputs import get_gamepad
 import time
 
 prev_time = time.perf_counter() * 1000000
+last_event_code = None
 
 while True:
     events = get_gamepad()
-    for event in events:
-        current_time = time.perf_counter() * 1000000
-        time_diff = current_time - prev_time
-        prev_time = current_time
-        print("Time difference:", time_diff, "μs")
+    if events:
+        event = events[0]
+        event_code = (event.ev_type, event.code)
+        
+        if event_code == ('Sync', 'SYN_REPORT'):
+            last_event_code = event_code
+        
+        if event_code != last_event_code:
+            current_time = time.perf_counter() * 1000000
+            time_diff = current_time - prev_time
+            prev_time = current_time
+            print("Time difference:", time_diff, "μs", event_code)
+            last_event_code = event_code
