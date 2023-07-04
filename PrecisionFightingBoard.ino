5aec48466b0b43922747128a387a7d113ae30e14
iori kojima <iori@iori.dev>
2023-07-04 16:37:14 +0900

    Update handle button bounce

--- PrecisionFightingBoard.ino
+++ PrecisionFightingBoard.ino
@@ -1,4 +1,5 @@
 #include <XInput.h>
+#include <Bounce2.h>
 
 const int Pin_ButtonA = 0;
 const int Pin_ButtonB = 1;
@@ -21,6 +22,28 @@ const int Pin_DpadRight = 13;
 
 const int Pin_TriggerL = A4;
 const int Pin_TriggerR = A5;
+
+Bounce buttonA = Bounce();
+Bounce buttonB = Bounce();
+Bounce buttonX = Bounce();
+Bounce buttonY = Bounce();
+
+Bounce buttonLB = Bounce();
+Bounce buttonRB = Bounce();
+
+Bounce buttonBack = Bounce();
+Bounce buttonStart = Bounce();
+
+Bounce buttonL3 = Bounce();
+Bounce buttonR3 = Bounce();
+
+Bounce dpadUp = Bounce();
+Bounce dpadDown = Bounce();
+Bounce dpadLeft = Bounce();
+Bounce dpadRight = Bounce();
+
+Bounce triggerLeft = Bounce();
+Bounce triggerRight = Bounce();
 
 void setup()
 {
@@ -47,6 +70,50 @@ void setup()
   pinMode(Pin_DpadLeft, INPUT_PULLUP);
   pinMode(Pin_DpadRight, INPUT_PULLUP);
 
+  buttonA.attach(Pin_ButtonA, INPUT_PULLUP);
+  buttonB.attach(Pin_ButtonB, INPUT_PULLUP);
+  buttonX.attach(Pin_ButtonX, INPUT_PULLUP);
+  buttonY.attach(Pin_ButtonY, INPUT_PULLUP);
+
+  buttonLB.attach(Pin_ButtonLB, INPUT_PULLUP);
+  buttonRB.attach(Pin_ButtonRB, INPUT_PULLUP);
+
+  buttonBack.attach(Pin_ButtonBack, INPUT_PULLUP);
+  buttonStart.attach(Pin_ButtonStart, INPUT_PULLUP);
+
+  buttonL3.attach(Pin_ButtonL3, INPUT_PULLUP);
+  buttonR3.attach(Pin_ButtonR3, INPUT_PULLUP);
+
+  dpadUp.attach(Pin_DpadUp, INPUT_PULLUP);
+  dpadDown.attach(Pin_DpadDown, INPUT_PULLUP);
+  dpadLeft.attach(Pin_DpadLeft, INPUT_PULLUP);
+  dpadRight.attach(Pin_DpadRight, INPUT_PULLUP);
+
+  triggerLeft.attach(Pin_TriggerL, INPUT_PULLUP);
+  triggerRight.attach(Pin_TriggerR, INPUT_PULLUP);
+
+  buttonA.interval(1);
+  buttonB.interval(1);
+  buttonX.interval(1);
+  buttonY.interval(1);
+
+  buttonLB.interval(1);
+  buttonRB.interval(1);
+
+  buttonBack.interval(1);
+  buttonStart.interval(1);
+
+  buttonL3.interval(1);
+  buttonR3.interval(1);
+
+  dpadUp.interval(1);
+  dpadDown.interval(1);
+  dpadLeft.interval(1);
+  dpadRight.interval(1);
+
+  triggerLeft.interval(1);
+  triggerRight.interval(1);
+
   XInput.setAutoSend(false);
 
   XInput.begin();
@@ -54,58 +121,58 @@ void setup()
 
 void loop()
 {
-  boolean buttonA = !digitalRead(Pin_ButtonA);
-  boolean buttonB = !digitalRead(Pin_ButtonB);
-  boolean buttonX = !digitalRead(Pin_ButtonX);
-  boolean buttonY = !digitalRead(Pin_ButtonY);
+  buttonA.update();
+  buttonB.update();
+  buttonX.update();
+  buttonY.update();
 
-  boolean buttonLB = !digitalRead(Pin_ButtonLB);
-  boolean buttonRB = !digitalRead(Pin_ButtonRB);
+  buttonLB.update();
+  buttonRB.update();
 
-  boolean buttonBack = !digitalRead(Pin_ButtonBack);
-  boolean buttonStart = !digitalRead(Pin_ButtonStart);
+  buttonBack.update();
+  buttonStart.update();
 
-  boolean buttonL3 = !digitalRead(Pin_ButtonL3);
-  boolean buttonR3 = !digitalRead(Pin_ButtonR3);
+  buttonL3.update();
+  buttonR3.update();
 
-  boolean dpadUp = !digitalRead(Pin_DpadUp);
-  boolean dpadDown = !digitalRead(Pin_DpadDown);
-  boolean dpadLeft = !digitalRead(Pin_DpadLeft);
-  boolean dpadRight = !digitalRead(Pin_DpadRight);
+  dpadUp.update();
+  dpadDown.update();
+  dpadLeft.update();
+  dpadRight.update();
 
-  XInput.setButton(BUTTON_A, buttonA);
-  XInput.setButton(BUTTON_B, buttonB);
-  XInput.setButton(BUTTON_X, buttonX);
-  XInput.setButton(BUTTON_Y, buttonY);
+  triggerLeft.update();
+  triggerRight.update();
 
-  XInput.setButton(BUTTON_LB, buttonLB);
-  XInput.setButton(BUTTON_RB, buttonRB);
+  XInput.setButton(BUTTON_A, buttonA.read());
+  XInput.setButton(BUTTON_B, buttonB.read());
+  XInput.setButton(BUTTON_X, buttonX.read());
+  XInput.setButton(BUTTON_Y, buttonY.read());
 
-  XInput.setButton(BUTTON_BACK, buttonBack);
-  XInput.setButton(BUTTON_START, buttonStart);
+  XInput.setButton(BUTTON_LB, buttonLB.read());
+  XInput.setButton(BUTTON_RB, buttonRB.read());
 
-  XInput.setButton(BUTTON_L3, buttonL3);
-  XInput.setButton(BUTTON_R3, buttonR3);
+  XInput.setButton(BUTTON_BACK, buttonBack.read());
+  XInput.setButton(BUTTON_START, buttonStart.read());
+
+  XInput.setButton(BUTTON_L3, buttonL3.read());
+  XInput.setButton(BUTTON_R3, buttonR3.read());
 
   // SOCD
-  if (dpadRight && dpadLeft)
+  if (dpadRight.read() && dpadLeft.read())
   {
-    dpadRight = false;
-    dpadLeft = false;
+    dpadRight.write(false);
+    dpadLeft.write(false);
   }
-  if (dpadUp && dpadDown)
+  if (dpadUp.read() && dpadDown.read())
   {
-    dpadUp = false;
-    dpadDown = false;
+    dpadUp.write(false);
+    dpadDown.write(false);
   }
 
-  XInput.setDpad(dpadUp, dpadDown, dpadLeft, dpadRight);
+  XInput.setDpad(dpadUp.read(), dpadDown.read(), dpadLeft.read(), dpadRight.read());
 
-  boolean triggerLeft = !digitalRead(Pin_TriggerL);
-  boolean triggerRight = !digitalRead(Pin_TriggerR);
-
-  XInput.setButton(TRIGGER_LEFT, triggerLeft);
-  XInput.setButton(TRIGGER_RIGHT, triggerRight);
+  XInput.setButton(TRIGGER_LEFT, triggerLeft.read());
+  XInput.setButton(TRIGGER_RIGHT, triggerRight.read());
 
   XInput.send();
 }
