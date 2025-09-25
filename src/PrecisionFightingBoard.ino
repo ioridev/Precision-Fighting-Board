e2d7bee5df5ce8c2d2a8ad91b197e348c7a0c19c
Iori Kojima <iori@iori.dev>
2025-09-25 16:53:25 +0900

    Add hall effect D-pad support

--- src/PrecisionFightingBoard.ino
+++ src/PrecisionFightingBoard.ino
@@ -1,6 +1,150 @@
 #include <XInput.h>
 #include <Bounce2.h>
 
+namespace {
+
+constexpr int kHallKeyCount = 4;
+constexpr int kHallPins[kHallKeyCount] = {14, 15, 16, 17};
+constexpr float kHallTriggerRatio = 0.05f;   // 0.2 mm / 4 mm travel
+constexpr float kHallReleaseRatio = 0.10f;   // 0.4 mm / 4 mm travel
+constexpr int kHallCalibrationSamples = 200;
+constexpr int kHallCalibrationDelayMs = 1;
+constexpr int kHallMinimumRange = 60;
+constexpr int kHallDefaultRange = 160;
+constexpr int kHallMinimumTrigger = 6;
+constexpr int kHallReleaseSeparation = 4;
+
+enum HallDirection {
+  HallUp = 0,
+  HallDown,
+  HallLeft,
+  HallRight
+};
+
+struct HallKey {
+  int pin;
+  int minValue;
+  int maxValue;
+  int trigger;
+  int release;
+  int value;
+  int reference;
+  bool pressed;
+  bool hasFullTravel;
+};
+
+HallKey hallKeys[kHallKeyCount];
+
+void updateHallThresholds(HallKey &key) {
+  int range = key.maxValue - key.minValue;
+  if (range < kHallMinimumRange) {
+    range = key.hasFullTravel ? kHallMinimumRange : kHallDefaultRange;
+  }
+
+  int trigger = static_cast<int>(range * kHallTriggerRatio);
+  if (trigger < kHallMinimumTrigger) {
+    trigger = kHallMinimumTrigger;
+  }
+
+  int release = static_cast<int>(range * kHallReleaseRatio);
+  if (release <= trigger) {
+    release = trigger + kHallReleaseSeparation;
+  }
+
+  if (release > range) {
+    release = range;
+  }
+
+  key.trigger = trigger;
+  key.release = release;
+}
+
+void initHallKey(int index) {
+  HallKey &key = hallKeys[index];
+  key.pin = kHallPins[index];
+  pinMode(key.pin, INPUT);
+
+  int minValue = 4095;
+  int maxValue = 0;
+
+  for (int i = 0; i < kHallCalibrationSamples; ++i) {
+    int reading = analogRead(key.pin);
+    if (reading < minValue) {
+      minValue = reading;
+    }
+    if (reading > maxValue) {
+      maxValue = reading;
+    }
+    delay(kHallCalibrationDelayMs);
+  }
+
+  key.minValue = minValue;
+  key.maxValue = maxValue;
+  key.reference = maxValue;
+  key.value = maxValue;
+  key.pressed = false;
+  key.hasFullTravel = false;
+
+  const int cushion = (maxValue - minValue) * 2;
+  key.minValue = max(0, key.minValue - cushion);
+  key.maxValue = min(4095, key.maxValue + cushion);
+  key.reference = key.maxValue;
+
+  updateHallThresholds(key);
+}
+
+void initHallKeys() {
+  for (int i = 0; i < kHallKeyCount; ++i) {
+    initHallKey(i);
+  }
+}
+
+void balanceHallKey(HallKey &key) {
+  if (key.pressed) {
+    if (key.value < key.minValue) {
+      key.minValue = key.value;
+      key.hasFullTravel = true;
+      updateHallThresholds(key);
+    }
+  } else {
+    if (key.value > key.maxValue) {
+      key.maxValue = key.value;
+      key.reference = key.maxValue;
+      updateHallThresholds(key);
+    }
+    if (key.value < key.minValue) {
+      key.minValue = key.value;
+      updateHallThresholds(key);
+    }
+  }
+}
+
+bool processHallKey(HallKey &key) {
+  key.value = analogRead(key.pin);
+  balanceHallKey(key);
+
+  if (!key.pressed) {
+    if (key.value <= key.reference - key.trigger) {
+      key.pressed = true;
+    }
+  } else {
+    if (key.value >= key.reference - key.release) {
+      key.pressed = false;
+      key.reference = key.maxValue;
+    }
+  }
+
+  return key.pressed;
+}
+
+void updateHallKeys() {
+  for (int i = 0; i < kHallKeyCount; ++i) {
+    processHallKey(hallKeys[i]);
+  }
+}
+
+} // namespace
+
 const int Pin_ButtonA = 11;
 const int Pin_ButtonB = 12;
 const int Pin_ButtonX = 8;
@@ -15,15 +159,15 @@ const int Pin_ButtonStart = 6;
 const int Pin_ButtonL3 =  18;
 const int Pin_ButtonR3 = 19;
 
-const int Pin_DpadUp = 0;
-const int Pin_DpadDown = 1;
-const int Pin_DpadLeft = 2;
-const int Pin_DpadRight = 3;
+const int Pin_DpadUp = kHallPins[HallUp];
+const int Pin_DpadDown = kHallPins[HallDown];
+const int Pin_DpadLeft = kHallPins[HallLeft];
+const int Pin_DpadRight = kHallPins[HallRight];
 
 const int Pin_ButtonXbox = 4;
 
-const int Pin_TriggerL = 14;
-const int Pin_TriggerR = 15;
+const int Pin_TriggerL = 20;
+const int Pin_TriggerR = 21;
 
 Bounce debouncerA = Bounce();
 Bounce debouncerB = Bounce();
@@ -38,11 +182,6 @@ Bounce debouncerStart = Bounce();
 
 Bounce debouncerL3 = Bounce();
 Bounce debouncerR3 = Bounce();
-
-Bounce debouncerDpadUp = Bounce();
-Bounce debouncerDpadDown = Bounce();
-Bounce debouncerDpadLeft = Bounce();
-Bounce debouncerDpadRight = Bounce();
 
 Bounce debouncerTriggerL = Bounce();
 Bounce debouncerTriggerR = Bounce();
@@ -65,15 +204,10 @@ void setup()
   pinMode(Pin_ButtonL3, INPUT_PULLUP);
   pinMode(Pin_ButtonR3, INPUT_PULLUP);
 
-  pinMode(Pin_DpadUp, INPUT_PULLUP);
-  pinMode(Pin_DpadDown, INPUT_PULLUP);
-  pinMode(Pin_DpadLeft, INPUT_PULLUP);
-  pinMode(Pin_DpadRight, INPUT_PULLUP);
-
   pinMode(Pin_TriggerL, INPUT_PULLUP);
   pinMode(Pin_TriggerR, INPUT_PULLUP);
 
-pinMode(Pin_ButtonXbox, INPUT_PULLUP);
+  pinMode(Pin_ButtonXbox, INPUT_PULLUP);
 
   debouncerA.attach(Pin_ButtonA);
   debouncerB.attach(Pin_ButtonB);
@@ -88,11 +222,6 @@ pinMode(Pin_ButtonXbox, INPUT_PULLUP);
 
   debouncerL3.attach(Pin_ButtonL3);
   debouncerR3.attach(Pin_ButtonR3);
-
-  debouncerDpadUp.attach(Pin_DpadUp);
-  debouncerDpadDown.attach(Pin_DpadDown);
-  debouncerDpadLeft.attach(Pin_DpadLeft);
-  debouncerDpadRight.attach(Pin_DpadRight);
 
   debouncerTriggerL.attach(Pin_TriggerL);
   debouncerTriggerR.attach(Pin_TriggerR);
@@ -114,15 +243,13 @@ pinMode(Pin_ButtonXbox, INPUT_PULLUP);
   debouncerL3.interval(3);
   debouncerR3.interval(3);
 
-  debouncerDpadUp.interval(3);
-  debouncerDpadDown.interval(3);
-  debouncerDpadLeft.interval(3);
-  debouncerDpadRight.interval(3);
-
   debouncerTriggerL.interval(3);
   debouncerTriggerR.interval(3);
 
-debouncerXbox.interval(3);
+  debouncerXbox.interval(3);
+
+  analogReadResolution(12);
+  initHallKeys();
 
   XInput.setAutoSend(false);
   XInput.begin();
@@ -144,15 +271,12 @@ void loop()
   debouncerL3.update();
   debouncerR3.update();
 
-  debouncerDpadUp.update();
-  debouncerDpadDown.update();
-  debouncerDpadLeft.update();
-  debouncerDpadRight.update();
-
   debouncerTriggerL.update();
   debouncerTriggerR.update();
 
   debouncerXbox.update();
+
+  updateHallKeys();
 
   boolean buttonA = !debouncerA.read();
   boolean buttonB = !debouncerB.read();
@@ -168,10 +292,10 @@ void loop()
   boolean buttonL3 = !debouncerL3.read();
   boolean buttonR3 = !debouncerR3.read();
 
-  boolean dpadUp = !debouncerDpadUp.read();
-  boolean dpadDown = !debouncerDpadDown.read();
-  boolean dpadLeft = !debouncerDpadLeft.read();
-  boolean dpadRight = !debouncerDpadRight.read();
+  boolean dpadUp = hallKeys[HallUp].pressed;
+  boolean dpadDown = hallKeys[HallDown].pressed;
+  boolean dpadLeft = hallKeys[HallLeft].pressed;
+  boolean dpadRight = hallKeys[HallRight].pressed;
 
   boolean buttonXbox = !debouncerXbox.read();
 
@@ -214,3 +338,4 @@ void loop()
 
   XInput.send();
 }
+
